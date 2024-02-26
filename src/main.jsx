b4d07627b8ab5b9c653ad165084f2f3dd8332351
qpatryk <patryk.nyczka@hotmail.com>
2024-02-26 20:41:30 +0100

    added Show.jsx and API coingecko and zustand hook and homeStore.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Show from './pages/Show';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
+        <Route path="/:id" element={<Show />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
